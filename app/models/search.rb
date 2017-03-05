e36181dba196adcf022f1d905af48988ec2edacd
jstevens00 <joeystevens00@gmail.com>
2017-03-04 21:45:59 -0500

    added another data source

--- app/models/search.rb
+++ app/models/search.rb
@@ -6,6 +6,7 @@ class Search
   TPB_URL="https://thepiratebay.org"
   EXTRA_TORRENT_URL="http://extratorrent.cc"
   KICKASS_TORRENTS_URL="http://kickasstorrents.to"
+  EZTV_URL="https://eztv.ag"
 
   def initialize(query)
     @query = query
@@ -34,14 +35,45 @@ class Search
     end
   end
 
+  def eztv
+      resp = {}
+      rank = 0
+      eztv_torrents=getUrl("#{EZTV_URL}/search/#{@query}")
+      eztv_torrents.css("tr.forum_header_border").each do |torrent|
+        name=torrent.css("a.epinfo").text
+        detail_page=torrent.css(".epinfo").attribute("href").text
+        detail_page="#{EZTV_URL}#{detail_page}}"
+        if not torrent.css("a.magnet").to_s.empty?
+          magnet=torrent.css("a.magnet").attribute("href").text
+        elsif not torrent.css("a.download_2").to_s.empty?
+          magnet=torrent.css("a.download_2").attribute("href").text
+        elsif not torrent.css("a.download_1").to_s.empty?
+          magnet=torrent.css("a.download_1").attribute("href").text
+        else
+          return false
+        end
+        size=torrent.text.split("\n").grep(/^[0-9].*/)[0]
+        seeders=torrent.text.split("\n").grep(/^[0-9]+$/)[0]
+        seeders
+        leechers=""
+        resp.store(name, [rank, magnet, detail_page, seeders, leechers, size])
+        rank+=1
+      end
+      resp
+  #  rescue => explode
+  #    p "ERROR: #{explode}" # Just throw any ol' error in explode
+  #    false
+    end
+  #end
+
   def kickass_torrents
     resp = {}
     rank = 0
     ka_torr=getUrl("#{KICKASS_TORRENTS_URL}/usearch/#{@query}/?rss=1")
     if ka_torr
       ka_torr.xpath("//item").each do |item|
-       seeders="?"
-       leechers="?"
+       seeders=""
+       leechers=""
        magnet=item.css("enclosure").attribute("url").text
        size=item.css("enclosure").attribute("length").text
        detail_page=item.css("guid").text
@@ -93,8 +125,10 @@ class Search
     # Response Object: { torret_name => torrent_attributes }
     response_object_kat = kickass_torrents
     response_object_tpb = tpb
+    response_object_eztv = eztv
     @search.store("TPB", response_object_tpb) if response_object_tpb
     @search.store("KickAss Torrents", response_object_kat) if response_object_kat
+    @search.store("EZTV", response_object_eztv) if response_object_eztv
     @search
   end
 end
